fc80b0f3b65f3f20042fdf04b6515fff17a4f919
singularity24601 <63631273+singularity24601@users.noreply.github.com>
2022-12-11 23:49:15 +1100

    Add files via upload

--- advent2.R
+++ advent2.R
@@ -7,57 +7,45 @@ t <- list() #a list of all my temporary variables because I don't want the works
 
 
 q11items <- list()
-q11items[["zero"]] <- (c(99, 67, 92, 61, 83, 64, 98))
-q11items[["one"]] <- (c(78, 74, 88, 89, 50))
-q11items[["two"]] <- (c(98, 91))
-q11items[["three"]] <- (c(59, 72, 94, 91, 79, 88, 94, 51))
-q11items[["four"]] <- (c(95, 72, 78))
-q11items[["five"]] <- (c(76))
-q11items[["six"]] <- (c(69, 60, 53, 89, 71, 88))
-q11items[["seven"]] <- (c(72, 54, 63, 80))
+q11items[["0"]] <- (c(99, 67, 92, 61, 83, 64, 98))
+q11items[["1"]] <- (c(78, 74, 88, 89, 50))
+q11items[["2"]] <- (c(98, 91))
+q11items[["3"]] <- (c(59, 72, 94, 91, 79, 88, 94, 51))
+q11items[["4"]] <- (c(95, 72, 78))
+q11items[["5"]] <- (c(76))
+q11items[["6"]] <- (c(69, 60, 53, 89, 71, 88))
+q11items[["7"]] <- (c(72, 54, 63, 80))
+# I reference entries in the list by strings because R lists have weird behaviour if I reference them by number
+# 1. I can't have an entry with index 0
+# 2. If one monkey runs out of items, it disappears completely and the remaining monkeys get renumbered
 
 q11checks <- list()
-q11checks[["zero"]] <- 0
-q11checks[["one"]] <- 0
-q11checks[["two"]] <- 0
-q11checks[["three"]] <- 0
-q11checks[["four"]] <- 0
-q11checks[["five"]] <- 0
-q11checks[["six"]] <- 0
-q11checks[["seven"]] <- 0
+for (i in 0:7) {
+  q11checks[[toString(i)]] <- 0  
+}
 
 q11functions <- list()
-q11functions[["zero"]] <- function(x) {return(x*17)}
-q11functions[["one"]] <- function(x) {return(x*11)}
-q11functions[["two"]] <- function(x) {return(x+4)}
-q11functions[["three"]] <- function(x) {return(x*x)}
-q11functions[["four"]] <- function(x) {return(x+7)}
-q11functions[["five"]] <- function(x) {return(x+8)}
-q11functions[["six"]] <- function(x) {return(x+5)}
-q11functions[["seven"]] <- function(x) {return(x+3)}
+q11functions[["0"]] <- function(x) {return(x*17)}
+q11functions[["1"]] <- function(x) {return(x*11)}
+q11functions[["2"]] <- function(x) {return(x+4)}
+q11functions[["3"]] <- function(x) {return(x*x)}
+q11functions[["4"]] <- function(x) {return(x+7)}
+q11functions[["5"]] <- function(x) {return(x+8)}
+q11functions[["6"]] <- function(x) {return(x+5)}
+q11functions[["7"]] <- function(x) {return(x+3)}
 
 q11tests <- list()
-q11tests[["zero"]] <- function(x) {return(x %% 3 == 0)}
-q11tests[["one"]] <- function(x) {return(x %% 5 == 0)}
-q11tests[["two"]] <- function(x) {return(x %% 2 == 0)}
-q11tests[["three"]] <- function(x) {return(x %% 13 == 0)}
-q11tests[["four"]] <- function(x) {return(x %% 11 == 0)}
-q11tests[["five"]] <- function(x) {return(x %% 17 == 0)}
-q11tests[["six"]] <- function(x) {return(x %% 19 == 0)}
-q11tests[["seven"]] <- function(x) {return(x %% 7 == 0)}
-
-q11mods <- list()
-q11mods[["zero"]] <- 3
-q11mods[["one"]] <- 5
-q11mods[["two"]] <- 2
-q11mods[["three"]] <- 13
-q11mods[["four"]] <- 11
-q11mods[["five"]] <- 17
-q11mods[["six"]] <- 19
-q11mods[["seven"]] <- 7
+q11tests[["0"]] <- function(x) {return(x %% 3 == 0)}
+q11tests[["1"]] <- function(x) {return(x %% 5 == 0)}
+q11tests[["2"]] <- function(x) {return(x %% 2 == 0)}
+q11tests[["3"]] <- function(x) {return(x %% 13 == 0)}
+q11tests[["4"]] <- function(x) {return(x %% 11 == 0)}
+q11tests[["5"]] <- function(x) {return(x %% 17 == 0)}
+q11tests[["6"]] <- function(x) {return(x %% 19 == 0)}
+q11tests[["7"]] <- function(x) {return(x %% 7 == 0)}
 
 
-q11m <- function(self="zero",option1="four",option2="two") {
+q11m <- function(self="0",option1="4",option2="2") {
   if (length(q11items[[self]]) == 0) {return()}
   for (i in 1:length(q11items[[self]])) { #inspect each item
     q11checks[[self]] <<- q11checks[[self]] + 1  
@@ -73,28 +61,20 @@ q11m <- function(self="zero",option1="four",option2="two") {
 }
 
 for (i in 1:20) {
-  q11m("zero","four","two")
-  q11m("one","three","five")
-  q11m("two","six","four")
-  q11m("three","zero","five")
-  q11m("four","seven","six")
-  q11m("five","zero","two")
-  q11m("six","seven","one")
-  q11m("seven","one","three")
+  q11m("0","4","2")
+  q11m("1","3","5")
+  q11m("2","6","4")
+  q11m("3","0","5")
+  q11m("4","7","6")
+  q11m("5","0","2")
+  q11m("6","7","1")
+  q11m("7","1","3")
 }
 
-a11 <- sort(unlist(q11checks))
+a11 <- rev(sort(unlist(q11checks)))
+a11 <- a11[1]*a11[2]
 
-q11items[["zero"]] <- (c(99, 67, 92, 61, 83, 64, 98))
-q11items[["one"]] <- (c(78, 74, 88, 89, 50))
-q11items[["two"]] <- (c(98, 91))
-q11items[["three"]] <- (c(59, 72, 94, 91, 79, 88, 94, 51))
-q11items[["four"]] <- (c(95, 72, 78))
-q11items[["five"]] <- (c(76))
-q11items[["six"]] <- (c(69, 60, 53, 89, 71, 88))
-q11items[["seven"]] <- (c(72, 54, 63, 80))
-
-q11m2 <- function(self="zero",option1="four",option2="two") {
+q11m2 <- function(self="0",option1="4",option2="2") {
   if (length(q11items[[self]]) == 0) {return()}
   for (i in 1:length(q11items[[self]])) { #inspect each item
     q11checks[[self]] <<- q11checks[[self]] + 1  
@@ -109,27 +89,29 @@ q11m2 <- function(self="zero",option1="four",option2="two") {
   q11items[[self]] <<- c() #no items left
 }
 
-q11checks[["zero"]] <- 0
-q11checks[["one"]] <- 0
-q11checks[["two"]] <- 0
-q11checks[["three"]] <- 0
-q11checks[["four"]] <- 0
-q11checks[["five"]] <- 0
-q11checks[["six"]] <- 0
-q11checks[["seven"]] <- 0
-
-cat("s")
-
-for (i in 1:10000) {
-  q11m2("zero","four","two")
-  q11m2("one","three","five")
-  q11m2("two","six","four")
-  q11m2("three","zero","five")
-  q11m2("four","seven","six")
-  q11m2("five","zero","two")
-  q11m2("six","seven","one")
-  q11m2("seven","one","three")
-  #print(q11items)
+for (i in 0:7) {
+  q11checks[[toString(i)]] <- 0  
 }
 
-a11b <- sort(unlist(q11checks))
+q11items[["0"]] <- (c(99, 67, 92, 61, 83, 64, 98))
+q11items[["1"]] <- (c(78, 74, 88, 89, 50))
+q11items[["2"]] <- (c(98, 91))
+q11items[["3"]] <- (c(59, 72, 94, 91, 79, 88, 94, 51))
+q11items[["4"]] <- (c(95, 72, 78))
+q11items[["5"]] <- (c(76))
+q11items[["6"]] <- (c(69, 60, 53, 89, 71, 88))
+q11items[["7"]] <- (c(72, 54, 63, 80))
+
+for (i in 1:10000) {
+  q11m2("0","4","2")
+  q11m2("1","3","5")
+  q11m2("2","6","4")
+  q11m2("3","0","5")
+  q11m2("4","7","6")
+  q11m2("5","0","2")
+  q11m2("6","7","1")
+  q11m2("7","1","3")
+}
+
+a11b <- rev(sort(unlist(q11checks)))
+a11b <- a11b[1]*a11b[2]
